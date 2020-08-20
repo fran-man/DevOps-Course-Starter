ef62b03b348463be0e537caa57a029e3f8c61e31
gsfranc <george.s.francis@aexp.com>
2020-08-20 12:55:39 +0100

    Review comments

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
-from session_items import get_items, get_item, add_item, save_item
+import session_items as session
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return renderIndex()
+    full_list = session.get_items()
+    return render_template('index.html', list=full_list)
 
-@app.route('/listitem', methods=['POST'])
+@app.route('/add-list-item', methods=['POST'])
 def addListItem():
     print("Adding Item!")
     print('Title=' + request.form.get('textbox'))
-    add_item(request.form.get('textbox'))
-    return renderIndex()
+    session.add_item(request.form.get('textbox'))
+    return redirect("/")
 
 @app.route('/completeditem', methods=['POST'])
 def updateListItem():
     print("Updating Item!")
     print(request.form.get('id'))
-    item_to_update = get_item(request.form.get('id'))
+    item_to_update = session.get_item(request.form.get('id'))
     print(item_to_update)
     item_to_update['status'] = 'Done!'
-    save_item(item_to_update)
-    return renderIndex()
-
-def renderIndex():
-    full_list = get_items()
-    return render_template('index.html', list=full_list)
+    session.save_item(item_to_update)
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run()
