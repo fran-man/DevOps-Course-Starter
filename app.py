9bb0cf38bc50cfbfd948eae82f713eff25bcce36
gsfranc <george.s.francis@aexp.com>
2020-08-28 15:19:53 +0100

    Initial changes to read from the trello API

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
+import os
 import session_items as session
+import requests
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+TRELLO_KEY = os.environ.get('TRELLO_KEY')
+TRELLO_TKN = os.environ.get('TRELLO_TKN')
+
+TRELLO_BOARD = '5f48fa1737fa77134ffe1e17'
+TRELLO_URL_BASE = 'https://api.trello.com/1/'
+
+
 @app.route('/')
 def index():
-    full_list = session.get_items()
-    return render_template('index.html', list=full_list)
+    params = {'key': TRELLO_KEY, 'token': TRELLO_TKN}
+    full_list = requests.get(TRELLO_URL_BASE + 'boards/' + TRELLO_BOARD + '/cards', data=params).json()
+    # print full_list
+    return render_template('index.html', list=trimCardsList(full_list))
+
 
 @app.route('/add-list-item', methods=['POST'])
 def addListItem():
@@ -15,6 +27,7 @@ def addListItem():
     print('Title=' + request.form.get('textbox'))
     session.add_item(request.form.get('textbox'))
     return redirect("/")
+
 
 @app.route('/completeditem', methods=['POST'])
 def updateListItem():
@@ -25,6 +38,15 @@ def updateListItem():
     item_to_update['status'] = 'Done!'
     session.save_item(item_to_update)
     return redirect("/")
+
+def trimCardsList(list_of_cards):
+    trimmed_result = []
+    for card in list_of_cards:
+        trimmed_result.append({
+            'name': card['name'],
+            'done': 'Complete!' if card['closed'] else 'Incomplete'
+        })
+    return trimmed_result
 
 if __name__ == '__main__':
     app.run()
