0a9e965f7598b23019d69faaa98596f7655e3a56
gsfranc <george.s.francis@aexp.com>
2020-09-01 17:42:09 +0100

    Sort by status and move some strings into constants.

--- trello_utils.py
+++ trello_utils.py
@@ -9,6 +9,9 @@ TRELLO_TODO_LIST = '5f48fa17b70abe80f7f0942d'
 TRELLO_DONE_LIST = '5f48fa17d5f15e744c86a725'
 TRELLO_URL_BASE = 'https://api.trello.com/1/'
 
+CARD_DONE_STATUS = 'Done!'
+CARD_TODO_STATUS = 'To-do'
+
 LOCAL_CARDS = []
 
 
@@ -19,13 +22,18 @@ def trimCardsList(trello_cards):
             card['name'],
             getCardStatus(card['id'])
         ))
+    print LOCAL_CARDS.sort(key=cardComparator)
     return LOCAL_CARDS
+
+
+def cardComparator(c):
+    return 1 if c.status == CARD_DONE_STATUS else 0
 
 
 def getCardStatus(card_id):
     params = {'key': TRELLO_KEY, 'token': TRELLO_TKN}
     board_name = requests.get(TRELLO_URL_BASE + 'cards/' + card_id + '/list', data=params).json()['name']
-    return 'Done!' if board_name == 'Done' else 'To-do'
+    return CARD_DONE_STATUS if board_name == 'Done' else CARD_TODO_STATUS
 
 
 class Card:
