5752265f087b999d990f8298a1ab1b2c6aacfc0d
fran-man <fran.man@hotmail.co.uk>
2021-07-09 12:09:09 +0100

    Module 12: Completed up to Step 3.

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+ required_providers {
+ azurerm = {
+ source = "hashicorp/azurerm"
+ version = ">= 2.49"
+ }
+ }
+}
+provider "azurerm" {
+ features {}
+}
+data "azurerm_resource_group" "main" {
+ name = "AmericanExpress2_GeorgeFrancis_ProjectExercise"
+}
+
+resource "azurerm_app_service_plan" "main" {
+ name = "george-devops-tf-asp"
+ location = data.azurerm_resource_group.main.location
+ resource_group_name = data.azurerm_resource_group.main.name
+ kind = "Linux"
+ reserved = true
+ sku {
+ tier = "Basic"
+ size = "B1"
+ }
+}
+resource "azurerm_app_service" "main" {
+ name = "george-devops-terraform"
+ location = data.azurerm_resource_group.main.location
+ resource_group_name = data.azurerm_resource_group.main.name
+ app_service_plan_id = azurerm_app_service_plan.main.id
+ site_config {
+ app_command_line = ""
+ linux_fx_version = "DOCKER|appsvcsample/python-helloworld:latest"
+ }
+ app_settings = {
+ "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+ }
+}
